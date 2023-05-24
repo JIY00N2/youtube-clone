c95669331d4e15b93616a44ad27659f6bf4c6afc
JiYoon <angella990825@gmail.com>
2023-05-24 23:53:39 +0900

    2023.05.24 fetch 대신 axios 라이브러리 사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import {useQuery} from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const {keyword} = useParams();
   // useQuery(캐시키, 어떻게 가지고 올건지 함수로 전달)
   // async() - 비동기 함수
   const {isLoading, error, data: videos} = useQuery(
-    ['videos',keyword], async () => {return fetch(`/videos/${keyword ? 'search' : 'poplular'}.json`)
-    .then(res => res.json())
-    .then(data => data.items);
+    ['videos',keyword], async () => {return axios.get(`/videos/${keyword ? 'search' : 'poplular'}.json`)
+    .then(res => res.data.items);
   });
   return (
     <>
