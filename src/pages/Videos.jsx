4a1fe8e21949ba1c2da02d1e507bea772f18d0b2
JiYoon <angella990825@gmail.com>
2023-05-25 00:31:49 +0900

    유튜브 api를 사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import {useQuery} from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import {search} from '../api/youtube';
+import Youtube, {search} from '../api/youtube';
 import FakeYoutube from '../api/fakeYoutube';
 
 
@@ -12,7 +12,7 @@ export default function Videos() {
   // async() - 비동기 함수
   const {isLoading, error, data: videos} = useQuery(
     ['videos',keyword], () => {
-      const youtube = new FakeYoutube();
+      const youtube = new Youtube();
       return youtube.search(keyword);
     });
   return (
