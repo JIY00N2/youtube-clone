8980c495e5ba7aec6728a036d7a51d8ca4d56aa3
JiYoon <angella990825@gmail.com>
2023-05-26 00:32:03 +0900

    오류해결

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,16 +1,16 @@
-export default class Youtube{
+export default class Youtube {
   constructor(apiClient) {
     this.apiClient = apiClient;
   }
 
-  async search(keyword){
+  async search(keyword) {
     return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
   }
 
-  async channelImageURL(id){
+  async channelImageURL(id) {
     return this.apiClient
-      .channels({params:{part:'snippet', id}})
-      .then(res=>res.data.items[0].snippet.thumbnails.default.url);
+      .channels({ params: { part: 'snippet', id } })
+      .then((res) => res.data.items[0].snippet.thumbnails.default.url);
   }
 
   async relatedVideos(id) {
@@ -28,30 +28,30 @@ export default class Youtube{
       );
   }
 
-  async #searchByKeyword(keyword){
+  async #searchByKeyword(keyword) {
     return this.apiClient
       .search({
-          params:{
-            part:'snippet',
-            maxResults: 25,
-            type: 'video',
-            q: keyword,
-          },
-        })
-        .then((res) => 
-          res.data.itmes.map(item=>({...item, id: item.id.videoId}))
-        );
-  } 
+        params: {
+          part: 'snippet',
+          maxResults: 25,
+          type: 'video',
+          q: keyword,
+        },
+      })
+      .then((res) =>
+        res.data.items.map((item) => ({ ...item, id: item.id.videoId }))
+      );
+  }
 
-  async #mostPopular(){
+  async #mostPopular() {
     return this.apiClient
       .videos({
-        params:{
-          part:'snippet',
+        params: {
+          part: 'snippet',
           maxResults: 25,
           chart: 'mostPopular',
         },
       })
-      .then(res => res.data.items);
-  } 
+      .then((res) => res.data.items);
+  }
 }
