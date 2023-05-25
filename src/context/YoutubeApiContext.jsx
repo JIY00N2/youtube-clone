1ea012656f77b7d207532fae8d38af7da9ae60da
JiYoon <angella990825@gmail.com>
2023-05-26 00:18:23 +0900

    최종 정리

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,14 +1,14 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutubeClient";
-import FakeYoutubeClient from "../api/fakeYoutubeClient";
+// import FakeYoutube from "../api/fakeYoutubeClient";
+// import FakeYoutubeClient from "../api/fakeYoutubeClient";
 import YoutubeClient from "../api/youtubeClient";
 
 // 우산 만들기, 어떤 유튜브 인스턴스를 사용할것인지 결정
 export const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient();
-// const client = new YoutubeClient();
+//const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({children}){
