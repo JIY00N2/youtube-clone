2c7b50a87cfc92186141dfc9adb6bd27e06cdf26
JiYoon <angella990825@gmail.com>
2023-05-26 00:01:07 +0900

    2023.05.26 유튜브 상세 페이지 꾸미기 (with tailwind)

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -15,12 +15,9 @@ export default function RelatedVideos({id}) {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😖</p>}
       {videos && (
-        // 기본적으로 grid로 만들고, 반응형으로 만들기 위해 화면이 작으면 1줄로 표시
-        // 화면이 small size를 넘어가면 2줄로 표시, 화면이 large size를 넘어가면 3줄로 표시 ...
-        // gap을 2씩 주고 수직으로는 gap을 4 주기
         <ul>
           {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
+            <VideoCard key={video.id} video={video} type='list'/>
           ))}
         </ul>
       )}
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { formatAgo } from '../util/date';
 import {useNavigate} from 'react-router-dom';
 
-export default function VideoCard({video}) {
+export default function VideoCard({video, type}) {
   const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
   const navigate = useNavigate();
+  const isList = type === 'list';
   return (
     <li 
+      className={isList ? 'flex gap-1 m-2' : ''}
       onClick={() => {
         navigate(`/videos/watch/${video.id}`, {state: {video}});
       }}
     >
       {/* width = full */}
-      <img className ='w-full' src = {thumbnails.medium.url} alt={title}></img>
+      <img className ={isList ? 'w-60 mr-2' : 'w-full'} src = {thumbnails.medium.url} alt={title}></img>
       <div>
         {/* my-2 = 위 아래로 margin 2 , line-clamp-2 : 줄 갯수를 최대 2줄로 제한*/}
         <p className ='font-semibold my-2 line-clamp-2'>{title}</p>
