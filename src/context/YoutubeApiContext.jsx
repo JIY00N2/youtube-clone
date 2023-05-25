8edaef902c7115766703b3bad71e212659d56f25
JiYoon <angella990825@gmail.com>
2023-05-25 22:07:27 +0900

    네트워크 통신을 다른 구현상황 별로 만들기(context 활용), 리팩토링

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,15 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
+import FakeYoutube from "../api/fakeYoutubeClient";
+import FakeYoutubeClient from "../api/fakeYoutubeClient";
+import YoutubeClient from "../api/youtubeClient";
 
 // 우산 만들기, 어떤 유튜브 인스턴스를 사용할것인지 결정
 export const YoutubeApiContext = createContext();
 
-const youtube = new FakeYoutube();
+const client = new FakeYoutubeClient();
+// const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({children}){
   return (
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {useQuery} from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import Youtube, {search} from '../api/youtube';
-import FakeYoutube from '../api/fakeYoutube';
+import Youtube, { search } from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutubeClient';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 
-
 export default function Videos() {
-  const {keyword} = useParams();
-  const {youtube} = useYoutubeApi();
+  const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   // useQuery(캐시키, 어떻게 가지고 올건지 함수로 전달)
   // async() - 비동기 함수
-  const {isLoading, error, data: videos} = useQuery(['videos',keyword], () => youtube.search(keyword));
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
+
   return (
     <>
-      <div>Videos {keyword ? `💡${keyword}` : '🔥'}</div>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
       {isLoading && <p>Loading...</p>}
-      {error && <p>Something is wrong</p>}
-      {videos && <ul>
-        {videos.map(video => <VideoCard key={video.id} video={video}></VideoCard>)}
-        </ul>}
+      {error && <p>Something is wrong 😖</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
     </>
-    
   );
 }
 
